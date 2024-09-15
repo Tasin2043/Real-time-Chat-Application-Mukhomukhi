160538c3200db9b1a63a559257c3b4568786a6c2
Tasin Islam <tasin2043@gmail.com>
2024-09-15 20:01:23 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -26,9 +26,9 @@ app.use("/api/message", messageRoutes)
 
 // --------------------------deployment------------------------------
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/client/build/index.html"))
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 );
 
 // const __dirname1 = path.resolve();
